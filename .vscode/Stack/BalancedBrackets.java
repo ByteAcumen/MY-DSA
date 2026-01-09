68b8dc41a188dfec12d67351cec33d011a4a3225
ByteAcumen <hemantharish04@gmail.com>
2026-01-09 22:23:43 +0530

    Balanced Stack

---
+++ .vscode/Stack/BalancedBrackets.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class BalancedBrackets {
+
+    public static boolean isBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
+            }
+            else if (ch == ')') {
+                if (stack.isEmpty()) return false;
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(isBalanced(str));
+    }
+}
