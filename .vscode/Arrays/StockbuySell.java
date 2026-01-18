5f1ffb32d784f81a78e09f0fd511406730f8b6f9
ByteAcumen <hemantharish04@gmail.com>
2026-01-18 19:02:32 +0530

    Stocks Buy and Sell

---
+++ .vscode/Arrays/StockbuySell.java
@@ -0,0 +1,30 @@
+public class StockbuySell {
+
+    public int stockbuyandSell(int[] prices) {
+        // int maxProfit = 0;
+        // for (int i = 0; i < prices.length; i++) {
+        // for (int j = i + 1; j < prices.length; j++) {
+        // int profit = prices[j] - prices[i];
+        // maxProfit = Math.max(maxProfit, profit);
+        // }
+        // }
+        // return maxProfit;
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        StockbuySell sol = new StockbuySell();
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        System.out.println("Max Profit: " + sol.stockbuyandSell(prices));
+    }
+
+}
