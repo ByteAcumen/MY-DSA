41e921fc8c7b8e044f3bf0b508a8888b8636c672
ByteAcumen <hemantharish04@gmail.com>
2026-01-24 21:59:44 +0530

    SetZeroes

--- .vscode/Arrays/SetZeroes.java
+++ .vscode/Arrays/SetZeroes.java
@@ -1,33 +1,53 @@
 public class SetZeroes {
 
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        for(int i = 0; i < n ; i++){
-            for(int j = 0; j < m ;j++){
-                if(matrix[i][j] == 0){
-                    for(int col = 0;col < n;col++){
-                        if(matrix[i][col] != 0){
-                            matrix[i][col] = -1;
-                        }
-                    }
-                    for(int row = 0 ; row < m ; row++){
-                        if(matrix[row][j] != 0){
-                            matrix[row][j] = -1;
-                        }
-                    }
+        // int m = matrix.length;
+        // int n = matrix[0].length;
+        // for(int i = 0; i < n ; i++){
+        // for(int j = 0; j < m ;j++){
+        // if(matrix[i][j] == 0){
+        // for(int col = 0;col < n;col++){
+        // if(matrix[i][col] != 0){
+        // matrix[i][col] = -1;
+        // }
+        // }
+        // for(int row = 0 ; row < m ; row++){
+        // if(matrix[row][j] != 0){
+        // matrix[row][j] = -1;
+        // }
+        // }
+        // }
+        // }
+        // }
+        // for(int i = 0;i < n; i++){
+        // for(int j = 0;j < m; j++){
+        // if(matrix[i][j] == -1){
+        // matrix[i][j] = 0;
+        // }
+        // }
+        // }
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
                 }
             }
         }
-        for(int i = 0;i < n; i++){
-            for(int j = 0;j < m; j++){
-                if(matrix[i][j] == -1){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] || col[j]) {
                     matrix[i][j] = 0;
                 }
             }
         }
-        
     }
+
     public static void main(String[] args) {
         int[][] matrix = {
                 { 1, 1, 1 },
